64711baa4a653264e2da26083c4dbcc5d89af0c5
Nathanael DEMACON <ndemacon@scaleway.com>
2023-01-27 15:07:11 +0100

    feat(core): export all regions and zones

--- scaleway-core/scaleway_core/bridge/region.py
+++ scaleway-core/scaleway_core/bridge/region.py
@@ -2,3 +2,9 @@ Region = str
 REGION_FR_PAR = Region("fr-par")
 REGION_NL_AMS = Region("nl-ams")
 REGION_PL_WAW = Region("pl-waw")
+
+ALL_REGIONS = [
+    REGION_FR_PAR,
+    REGION_NL_AMS,
+    REGION_PL_WAW,
+]
--- scaleway-core/scaleway_core/bridge/zone.py
+++ scaleway-core/scaleway_core/bridge/zone.py
@@ -6,3 +6,13 @@ ZONE_NL_AMS_1 = Zone("nl-ams-1")
 ZONE_NL_AMS_2 = Zone("nl-ams-2")
 ZONE_PL_WAW_1 = Zone("pl-waw-1")
 ZONE_PL_WAW_2 = Zone("pl-waw-2")
+
+ALL_ZONES = [
+    ZONE_FR_PAR_1,
+    ZONE_FR_PAR_2,
+    ZONE_FR_PAR_3,
+    ZONE_NL_AMS_1,
+    ZONE_NL_AMS_2,
+    ZONE_PL_WAW_1,
+    ZONE_PL_WAW_2,
+]
