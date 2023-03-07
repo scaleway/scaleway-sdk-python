714adc02fe496528d3d6af20e94cd824b11d2b0d
Nathanael Demacon <ndemacon@scaleway.com>
2023-03-07 14:42:13 +0100

    feat(instance): add server action utils (#75)

--- scaleway/scaleway/instance/v1/__init__.py
+++ scaleway/scaleway/instance/v1/__init__.py
@@ -1,5 +1,6 @@
 # This file was automatically generated. DO NOT EDIT.
 # If you have any remark or suggestion do not hesitate to open an issue.
+from typing import TYPE_CHECKING
 from .types import Arch
 from .types import BootType
 from .types import ImageState
@@ -114,6 +115,11 @@ from .content import TASK_TRANSIENT_STATUSES
 from .content import VOLUME_SERVER_TRANSIENT_STATUSES
 from .content import VOLUME_TRANSIENT_STATUSES
 from .api import InstanceV1API
+
+try:
+    from .api_utils import InstanceV1APIUtils as InstanceV1API  # type: ignore[attr-defined]
+except ImportError:
+    pass
 
 __all__ = [
     "Arch",
