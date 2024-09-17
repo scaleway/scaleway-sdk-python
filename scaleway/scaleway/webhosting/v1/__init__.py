e3fc254de591389edb4571264ed1d3113fba87f6
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2024-09-17 11:42:16 +0200

    feat(webhosting): add support for v1 API (#662)

---
+++ scaleway/scaleway/webhosting/v1/__init__.py
@@ -0,0 +1,153 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import HostingDnsStatus
+from .types import HostingStatus
+from .content import HOSTING_TRANSIENT_STATUSES
+from .types import HostingSummaryStatus
+from .content import HOSTING_SUMMARY_TRANSIENT_STATUSES
+from .types import ListDatabaseUsersRequestOrderBy
+from .types import ListDatabasesRequestOrderBy
+from .types import ListFtpAccountsRequestOrderBy
+from .types import ListHostingsRequestOrderBy
+from .types import ListMailAccountsRequestOrderBy
+from .types import ListOffersRequestOrderBy
+from .types import ListWebsitesRequestOrderBy
+from .types import OfferOptionName
+from .types import OfferOptionWarning
+from .types import OfferOption
+from .types import CreateHostingRequestDomainConfiguration
+from .types import OfferOptionRequest
+from .types import HostingCpanelUrls
+from .types import HostingOption
+from .types import ControlPanel
+from .types import DatabaseUser
+from .types import Database
+from .types import FtpAccount
+from .types import HostingSummary
+from .types import MailAccount
+from .types import Offer
+from .types import Website
+from .types import ControlPanelApiListControlPanelsRequest
+from .types import DatabaseApiAssignDatabaseUserRequest
+from .types import DatabaseApiChangeDatabaseUserPasswordRequest
+from .types import DatabaseApiCreateDatabaseRequest
+from .types import DatabaseApiCreateDatabaseUserRequest
+from .types import DatabaseApiDeleteDatabaseRequest
+from .types import DatabaseApiDeleteDatabaseUserRequest
+from .types import DatabaseApiGetDatabaseRequest
+from .types import DatabaseApiGetDatabaseUserRequest
+from .types import DatabaseApiListDatabaseUsersRequest
+from .types import DatabaseApiListDatabasesRequest
+from .types import DatabaseApiUnassignDatabaseUserRequest
+from .types import FtpAccountApiChangeFtpAccountPasswordRequest
+from .types import FtpAccountApiCreateFtpAccountRequest
+from .types import FtpAccountApiListFtpAccountsRequest
+from .types import FtpAccountApiRemoveFtpAccountRequest
+from .types import Hosting
+from .types import HostingApiCreateHostingRequest
+from .types import HostingApiCreateSessionRequest
+from .types import HostingApiDeleteHostingRequest
+from .types import HostingApiGetHostingRequest
+from .types import HostingApiListHostingsRequest
+from .types import HostingApiResetHostingPasswordRequest
+from .types import HostingApiUpdateHostingRequest
+from .types import ListControlPanelsResponse
+from .types import ListDatabaseUsersResponse
+from .types import ListDatabasesResponse
+from .types import ListFtpAccountsResponse
+from .types import ListHostingsResponse
+from .types import ListMailAccountsResponse
+from .types import ListOffersResponse
+from .types import ListWebsitesResponse
+from .types import MailAccountApiChangeMailAccountPasswordRequest
+from .types import MailAccountApiCreateMailAccountRequest
+from .types import MailAccountApiListMailAccountsRequest
+from .types import MailAccountApiRemoveMailAccountRequest
+from .types import OfferApiListOffersRequest
+from .types import ResetHostingPasswordResponse
+from .types import Session
+from .types import WebsiteApiListWebsitesRequest
+from .api import WebhostingV1ControlPanelAPI
+from .api import WebhostingV1DatabaseAPI
+from .api import WebhostingV1OfferAPI
+from .api import WebhostingV1HostingAPI
+from .api import WebhostingV1FtpAccountAPI
+from .api import WebhostingV1MailAccountAPI
+from .api import WebhostingV1WebsiteAPI
+
+__all__ = [
+    "HostingDnsStatus",
+    "HostingStatus",
+    "HOSTING_TRANSIENT_STATUSES",
+    "HostingSummaryStatus",
+    "HOSTING_SUMMARY_TRANSIENT_STATUSES",
+    "ListDatabaseUsersRequestOrderBy",
+    "ListDatabasesRequestOrderBy",
+    "ListFtpAccountsRequestOrderBy",
+    "ListHostingsRequestOrderBy",
+    "ListMailAccountsRequestOrderBy",
+    "ListOffersRequestOrderBy",
+    "ListWebsitesRequestOrderBy",
+    "OfferOptionName",
+    "OfferOptionWarning",
+    "OfferOption",
+    "CreateHostingRequestDomainConfiguration",
+    "OfferOptionRequest",
+    "HostingCpanelUrls",
+    "HostingOption",
+    "ControlPanel",
+    "DatabaseUser",
+    "Database",
+    "FtpAccount",
+    "HostingSummary",
+    "MailAccount",
+    "Offer",
+    "Website",
+    "ControlPanelApiListControlPanelsRequest",
+    "DatabaseApiAssignDatabaseUserRequest",
+    "DatabaseApiChangeDatabaseUserPasswordRequest",
+    "DatabaseApiCreateDatabaseRequest",
+    "DatabaseApiCreateDatabaseUserRequest",
+    "DatabaseApiDeleteDatabaseRequest",
+    "DatabaseApiDeleteDatabaseUserRequest",
+    "DatabaseApiGetDatabaseRequest",
+    "DatabaseApiGetDatabaseUserRequest",
+    "DatabaseApiListDatabaseUsersRequest",
+    "DatabaseApiListDatabasesRequest",
+    "DatabaseApiUnassignDatabaseUserRequest",
+    "FtpAccountApiChangeFtpAccountPasswordRequest",
+    "FtpAccountApiCreateFtpAccountRequest",
+    "FtpAccountApiListFtpAccountsRequest",
+    "FtpAccountApiRemoveFtpAccountRequest",
+    "Hosting",
+    "HostingApiCreateHostingRequest",
+    "HostingApiCreateSessionRequest",
+    "HostingApiDeleteHostingRequest",
+    "HostingApiGetHostingRequest",
+    "HostingApiListHostingsRequest",
+    "HostingApiResetHostingPasswordRequest",
+    "HostingApiUpdateHostingRequest",
+    "ListControlPanelsResponse",
+    "ListDatabaseUsersResponse",
+    "ListDatabasesResponse",
+    "ListFtpAccountsResponse",
+    "ListHostingsResponse",
+    "ListMailAccountsResponse",
+    "ListOffersResponse",
+    "ListWebsitesResponse",
+    "MailAccountApiChangeMailAccountPasswordRequest",
+    "MailAccountApiCreateMailAccountRequest",
+    "MailAccountApiListMailAccountsRequest",
+    "MailAccountApiRemoveMailAccountRequest",
+    "OfferApiListOffersRequest",
+    "ResetHostingPasswordResponse",
+    "Session",
+    "WebsiteApiListWebsitesRequest",
+    "WebhostingV1ControlPanelAPI",
+    "WebhostingV1DatabaseAPI",
+    "WebhostingV1OfferAPI",
+    "WebhostingV1HostingAPI",
+    "WebhostingV1FtpAccountAPI",
+    "WebhostingV1MailAccountAPI",
+    "WebhostingV1WebsiteAPI",
+]
