ca279964179dcc209ae38ca74b137fc300a780fd
Rémy Léone <rleone@scaleway.com>
2024-10-04 11:16:26 +0200

    fix: fix ruff lint configuration

--- scaleway-core/tests/test_bridge_marshal.py
+++ scaleway-core/tests/test_bridge_marshal.py
@@ -3,11 +3,12 @@ from decimal import Decimal
 
 from scaleway_core.bridge import unmarshal_Decimal, marshal_Decimal
 
+
 class TestBridgeMarshal(unittest.TestCase):
     def test_decimal_marshal(self):
-        decimal = Decimal('1.2')
-        self.assertEqual(marshal_Decimal(decimal), {'value': '1.2'})
+        decimal = Decimal("1.2")
+        self.assertEqual(marshal_Decimal(decimal), {"value": "1.2"})
 
     def test_decimal_unmarshal(self):
-        decimal = Decimal('1.2')
-        self.assertEqual(unmarshal_Decimal({'value': '1.2'}), decimal)
+        decimal = Decimal("1.2")
+        self.assertEqual(unmarshal_Decimal({"value": "1.2"}), decimal)
