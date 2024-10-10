e37037b6abae03465382bf2d3b06414af3ab2612
Laure-di <62625835+Laure-di@users.noreply.github.com>
2024-10-10 08:30:54 -0700

    feat(baremetal): add Private Network (#689)

---
+++ scaleway-async/scaleway_async/baremetal/v3/content.py
@@ -0,0 +1,15 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from typing import List
+
+from .types import (
+    ServerPrivateNetworkStatus,
+)
+
+SERVER_PRIVATE_NETWORK_TRANSIENT_STATUSES: List[ServerPrivateNetworkStatus] = [
+    ServerPrivateNetworkStatus.ATTACHING,
+    ServerPrivateNetworkStatus.DETACHING,
+]
+"""
+Lists transient statutes of the enum :class:`ServerPrivateNetworkStatus <ServerPrivateNetworkStatus>`.
+"""
