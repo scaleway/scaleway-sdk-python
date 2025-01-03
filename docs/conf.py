32e65101d46b3af9dea3ae12735f2992da30f782
Laure-di <masson.laure.l@gmail.com>
2025-01-03 14:50:14 +0100

    feat(doc): generate documentations

--- docs/conf.py
+++ docs/conf.py
@@ -6,32 +6,55 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-
-
-project = "Scaleway Python SDK"
-copyright = "2022, Scaleway"
-author = "Scaleway"
+project = 'Scaleway SDK Python'
+copyright = '2025, Scaleway'
+author = 'Scaleway'
+release = '2.7.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../scaleway'))
+sys.path.insert(0, os.path.abspath('../scaleway-async'))
+sys.path.insert(0, os.path.abspath('../scaleway-core'))
+
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
-autosummary_generate = True
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autoapi_dirs = [
+    os.path.abspath('../scaleway'),
+    # os.path.abspath('../scaleway-async'),
+    # os.path.abspath('../scaleway-core'),
+]
 
+autoapi_generate_api_docs = True
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/marshalling*']
+
+autodoc_default_options = {
+    "show-inheritance": True,
+    "members": True,
+    "undoc-members": False,
+}
+
+source_suffix = ".rst"
+
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_theme = 'furo'
+html_static_path = ['_static']
 
-sys.path.insert(0, os.path.abspath(".."))
+
