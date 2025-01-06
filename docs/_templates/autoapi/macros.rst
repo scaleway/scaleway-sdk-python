891fd081738c9fb9cd814603442b863ce73eaa9f
Laure-di <masson.laure.l@gmail.com>
2025-01-06 16:26:20 +0100

    sphinx init and templates

---
+++ docs/_templates/autoapi/macros.rst
@@ -0,0 +1,12 @@
+{% import 'macros.rst' as macros %}
+{% macro auto_summary(objs, title='') -%}
+
+.. list-table:: {{ title }}
+    :header-rows: 0
+    :widths: auto
+
+{% for obj in objs %}
+    * - obj.name
+      - obj.summary
+{% endfor %}
+{% endmacro %}
---
+++ docs/conf.py
@@ -0,0 +1,64 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Scaleway SDK Python'
+copyright = '2025, Scaleway'
+author = 'Scaleway'
+release = '2.7.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import re
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "autoapi.extension"
+]
+
+templates_path = ["_templates"]
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+autoapi_type = "python"
+autoapi_dirs = [
+    os.path.abspath('../scaleway'),
+    os.path.abspath('../scaleway-async'),
+    os.path.abspath('../scaleway-core'),
+]
+autoapi_template_dir = "_templates/autoapi"
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
+autoapi_generate_api_docs = True
+
+def skip_submodules(app, what, name, obj, skip, options):
+    if re.search("marshal*", name) or re.search("unmarshal*", name) or re.search("test*", name) or re.search("utils", name):
+        print("value of name: ", name, " obj: ", obj, "what: ", what)
+        skip = True
+    return skip
+
+
+def setup(sphinx):
+    sphinx.connect("autoapi-skip-member", skip_submodules)
