c41f7d2618773f5b14a34497b864c5cdb04255ba
Laure-di <lmasson@scaleway.com>
2025-07-03 17:47:33 +0200

    fix: type issue

--- scaleway-core/scaleway_core/utils/resolve_one_of.py
+++ scaleway-core/scaleway_core/utils/resolve_one_of.py
@@ -1,8 +1,5 @@
-from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, Dict, Generic, List, Optional, TypeVar
-
-from scaleway_core.profile import ProfileDefaults
 
 T = TypeVar("T")
 
@@ -10,33 +7,28 @@ T = TypeVar("T")
 @dataclass
 class OneOfPossibility(Generic[T]):
     param: str
+
     value: Optional[T]
-    default: Optional[T | ProfileDefaults] = None
-    marshal_func: Optional[Callable[[T, ProfileDefaults | None], Dict[str, Any]]] = None
+
+    default: Optional[T] = None
 
 
 def resolve_one_of(
     possibilities: List[OneOfPossibility[Any]], is_required: bool = False
-) -> dict[str, Any | None] | str | dict[Any, Any]:
+) -> Dict[str, Any]:
     """
     Resolves the ideal parameter and value amongst an optional list.
-    Uses marshal_func if provided.
     """
 
-    # Try to resolve using non-None value
+    # Get the first non-empty parameter
     for possibility in possibilities:
         if possibility.value is not None:
-            if possibility.marshal_func is not None:
-                return {
-                    possibility.param: possibility.marshal_func(
-                        possibility.value, possibility.default
-                    )
-                }
             return {possibility.param: possibility.value}
 
-    # Try to resolve using non-None default
+    # Get the first non-empty default
     for possibility in possibilities:
         if possibility.default is not None:
+<<<<<<< HEAD
             if possibility.marshal_func is not None:
                 # When no actual value, call with None as value
                 return {
@@ -44,14 +36,16 @@ def resolve_one_of(
                         None, possibility.default
                     )
                 }
+=======
+>>>>>>> 539c303 (Revert "fix(core): management one_of")
             return {possibility.param: possibility.default}
 
-    # If required but unresolved, raise an error
+    # If required, raise an error
     if is_required:
         possibilities_keys = " or ".join(
             [possibility.param for possibility in possibilities]
         )
         raise ValueError(f"one of ${possibilities_keys} must be present")
 
-    # Else, return empty dict
+    # Else, return an empty dict
     return {}
