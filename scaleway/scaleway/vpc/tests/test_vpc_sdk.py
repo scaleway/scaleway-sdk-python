a09ed3fe1b0e36f2de979d2e9cb082edbd7e9e07
Laure Masson <lmasson@scaleway.com>
2025-08-22 09:58:05 +0200

    tests: register cassette with auto-naming

--- scaleway/scaleway/vpc/tests/test_vpc_sdk.py
+++ scaleway/scaleway/vpc/tests/test_vpc_sdk.py
@@ -1,6 +1,5 @@
-import os
-
 import pytest
+
 from scaleway.vpc.v2 import VpcV2API
 from scaleway_core.api import ScalewayException
 from scaleway_core.client import Client
@@ -12,40 +11,40 @@ tags = ["sdk-python", "regression-test"]
 created_pn_count = 5
 created_vpc_count = 1
 
+
 @pytest.fixture(scope="module")
+@scw_vcr.use_cassette
 def vpc_api():
-    with scw_vcr.use_cassette("vpc_api.yaml"):
         client = Client.from_config_file_and_env()
         api = VpcV2API(client)
         return api, client.default_project_id, region
 
 
 @pytest.fixture(scope="module")
+@scw_vcr.use_cassette
 def vpc(vpc_api):
     api, project_id, region = vpc_api
-    with scw_vcr.use_cassette("vpc_create_fixture_setup.yaml"):
-        vpc = api.create_vpc(
-            enable_routing=True,
-            region=region,
-            project_id=project_id,
-            name=random_name("vpc-test-sdk-python"),
-        )
+    vpc = api.create_vpc(
+        enable_routing=True,
+        region=region,
+        project_id=project_id,
+        name=random_name("vpc-test-sdk-python"),
+    )
     yield vpc
-    with scw_vcr.use_cassette("vpc_delete_fixture_teardown.yaml"):
-        api.delete_vpc(vpc_id=vpc.id, region=region)
+    api.delete_vpc(vpc_id=vpc.id, region=region)
 
 
 @pytest.fixture
+@scw_vcr.use_cassette
 def private_networks_to_cleanup(vpc_api):
     api, _, _ = vpc_api
     items = []
     yield items
-    with scw_vcr.use_cassette("private_networks_delete_cleanup_fixture.yaml"):
-        for pn in items:
-            api.delete_private_network(private_network_id=pn.id)
+    for pn in items:
+        api.delete_private_network(private_network_id=pn.id)
 
 
-@scw_vcr.use_cassette("test_vpc_delete.yaml")
+@scw_vcr.use_cassette
 def test_vpc_delete(vpc_api):
     api, project_id, region = vpc_api
     vpc = api.create_vpc(
@@ -64,7 +63,7 @@ def test_vpc_delete(vpc_api):
         api.get_vpc(vpc_id=vpc.id)
 
 
-@scw_vcr.use_cassette("test_vpc_list.yaml")
+@scw_vcr.use_cassette()
 def test_vpc_list(vpc_api):
     api, _, region = vpc_api
     vpcs = api.list_vp_cs(region=region)
@@ -72,7 +71,7 @@ def test_vpc_list(vpc_api):
     assert vpcs.total_count >= created_vpc_count
 
 
-@scw_vcr.use_cassette("test_private_network_create.yaml")
+@scw_vcr.use_cassette()
 def test_private_network_create(vpc_api, vpc, private_networks_to_cleanup):
     api, project_id, _ = vpc_api
     for i in range(created_pn_count):
@@ -86,7 +85,7 @@ def test_private_network_create(vpc_api, vpc, private_networks_to_cleanup):
         assert pn.vpc_id == vpc.id
 
 
-@scw_vcr.use_cassette("test_private_network_list.yaml")
+@scw_vcr.use_cassette()
 def test_private_network_list(vpc_api):
     api, _, region = vpc_api
     networks = api.list_private_networks(region=region)
@@ -94,7 +93,7 @@ def test_private_network_list(vpc_api):
     assert networks.total_count >= created_pn_count
 
 
-@scw_vcr.use_cassette("test_vpc_get.yaml")
+@scw_vcr.use_cassette()
 def test_vpc_get(vpc_api, vpc):
     api, _, region = vpc_api
     fetched = api.get_vpc(vpc_id=vpc.id, region=region)
@@ -102,7 +101,7 @@ def test_vpc_get(vpc_api, vpc):
     assert fetched.id == vpc.id
 
 
-@scw_vcr.use_cassette("test_vpc_update.yaml")
+@scw_vcr.use_cassette()
 def test_vpc_update(vpc_api, vpc):
     api, _, _ = vpc_api
     updated = api.update_vpc(vpc_id=vpc.id, tags=tags)
@@ -110,7 +109,7 @@ def test_vpc_update(vpc_api, vpc):
     assert updated.id == vpc.id
 
 
-@scw_vcr.use_cassette("test_vpc_list_all.yaml")
+@scw_vcr.use_cassette()
 def test_vpc_list_all(vpc_api):
     api, _, _ = vpc_api
     vpcs = api.list_vp_cs_all()
