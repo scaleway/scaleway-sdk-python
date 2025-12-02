72e6d86e831b4973436a5e6ebc908945f3608d67
Laure-di <62625835+Laure-di@users.noreply.github.com>
2025-12-02 15:13:02 +0100

    Update scaleway-core/scaleway_core/utils/strenummeta.py
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- scaleway-core/scaleway_core/utils/strenummeta.py
+++ scaleway-core/scaleway_core/utils/strenummeta.py
@@ -3,14 +3,14 @@ from typing import Any
 
 
 class StrEnumMeta(EnumMeta):
-    def __call__(cls, value: Any, *values: Any, **kwargs: Any) -> Any:
+    def __call__(cls, value: str, *args: Any, **kwargs: Any) -> Any:
         names = kwargs.pop("names", None)
 
         if names is not None:
-            return super().__call__(value, names, *values, **kwargs)
+            return super().__call__(value, names, *args, **kwargs)
 
         try:
-            return super().__call__(value, *values, **kwargs)
+            return super().__call__(value, *args, **kwargs)
         except ValueError:
             if isinstance(value, str):
                 return value
