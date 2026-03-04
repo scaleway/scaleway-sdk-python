bcbc40ef1751b408cfcb0e80eeb971c47dd39b70
Yacine FODIL <yfodil@scaleway.com>
2026-03-04 14:39:31 +0100

    dont wrap None in Optional

--- scaleway/scaleway/instance/v1/custom_api.py
+++ scaleway/scaleway/instance/v1/custom_api.py
@@ -111,7 +111,7 @@ class InstanceUtilsV1API(InstanceV1API):
         server_id: str,
         user_data: Dict[str, bytes],
         zone: Optional[ScwZone] = None,
-    ) -> Optional[None]:
+    ) -> None:
         param_zone = validate_path_param("zone", zone or self.client.default_zone)
         param_server_id = validate_path_param("server_id", server_id)
 
