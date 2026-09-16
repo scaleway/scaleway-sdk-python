5adb58e9783ab4982d7b4bc7c62887ba8f30090d
Yacine Fodil <yfodil@scaleway.com>
2026-09-16 11:19:52 +0200

    add it-mil region

--- scaleway-core/scaleway_core/bridge/region.py
+++ scaleway-core/scaleway_core/bridge/region.py
@@ -2,9 +2,11 @@ Region = str
 REGION_FR_PAR = Region("fr-par")
 REGION_NL_AMS = Region("nl-ams")
 REGION_PL_WAW = Region("pl-waw")
+REGION_IT_MIL = Region("it-mil")
 
 ALL_REGIONS = [
     REGION_FR_PAR,
     REGION_NL_AMS,
     REGION_PL_WAW,
+    REGION_IT_MIL,
 ]
